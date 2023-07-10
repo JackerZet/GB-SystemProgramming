9e25ec2d44c7328aad4941aee6cfa7bd027f906d
Иванов Евгений <109151956+JackerZet@users.noreply.github.com>
2023-07-10 13:00:22 +0300

    homework-2

---
+++ Assets/Homeworks/Homework_2/Scripts/Jobs/PositionsJob.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+namespace Homeworks.homework_2
+{
+    internal struct PositionsJob : IJobParallelFor
+    {
+        [ReadOnly]
+        public NativeArray<Vector3> Positions;
+        [ReadOnly]
+        public NativeArray<Vector3> Velocities;
+        [WriteOnly]
+        public NativeArray<Vector3> FinalPositions;
+        public void Execute(int index)
+        {
+            FinalPositions[index] = Positions[index] + Velocities[index];
+        }
+    }
+}
